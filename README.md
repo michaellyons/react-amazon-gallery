052009540c00f173dd923ba811d41a1cbda420bf
Michael Lyons <m@gazeet.com>
2016-05-15 11:57:03 -0700

    Added sandbox gallery and removed material ui due to recent breaking changes, fallback to Bootstrap

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # react-amazon-gallery
+This is a React JS Component inspired by clean gallery design. 
+###Current Capabilities
+1. List Gallery View
+..* Renders a one dimensional array of image src strings.
+
+###Future Capabilities
+1. Variation Gallery View
+..* Allows for clean browsing experience of product variations and similar perspective combinations which would look terrible in a grid.
+..* Should have injectable image array option to render flavor options in custom/external div
--- sandbox/src/components/App.js
+++ sandbox/src/components/App.js
@@ -1,37 +1,60 @@
 import React from 'react';
-import Gallery from 'react-amazon-gallery';
-import baseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import RaisedButton from 'material-ui/RaisedButton';
-import Slider from 'material-ui/Slider';
-
+import PubGallery from 'react-amazon-gallery';
+import WorkingGallery from './Gallery';
 const DEFAULT_CONFIG = {
-  containerHeight: 400,
+  cHeight: 400,
   orientation: 'vertical',
   overlay: false, 
   posY: 'top',
-  jewelSpacing: 0.2,
-  jewelSize: 0.25,
-  hlColor: '#d55',
-  hlSize: 0.5,
-}
+  posX: 'left',
+  jewelSpacing: 8,
+  jewelSize: 42,
+  hlColor: '#ff8c00',
+  hlSize: 16,
+};
+if (!library)
+   var library = {};
+
+library.json = {
+   replacer: function(match, pIndent, pKey, pVal, pEnd) {
+      var key = '<span class="json-key">';
+      var val = '<span class="json-value">';
+      var str = '<span class="json-string">';
+      var r = pIndent || '';
+      if (pKey)
+         r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+      if (pVal)
+         r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
+      return r + (pEnd || '');
+      },
+   prettyPrint: function(obj) {
+      var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+      return JSON.stringify(obj, null, 3)
+         .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
+         .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+         .replace(jsonLine, library.json.replacer);
+      }
+   };
+   function createMarkup(markup) { return {__html: markup}; };
 var App = React.createClass({
   getInitialState() {
     return {
       ...DEFAULT_CONFIG
     }
   },
-  childContextTypes: {
-    muiTheme: React.PropTypes.function
+  resetDefault() {
+    this.setState({...DEFAULT_CONFIG});
   },
-  getChildContext() {
-    return {muiTheme: getMuiTheme(baseTheme)};
-  },
-
   changeConfig(config, val, e) {
     e.preventDefault();
     let {...newState} = this.state;
     newState[config] = val;
+    this.setState(newState);
+  },
+  handleTextChange(prop, e) {
+    e.preventDefault();
+    let {...newState} = this.state;
+    newState[prop] = e.target.value;
     this.setState(newState);
   },
   handleSlider(slider, value, e) {
@@ -44,6 +67,9 @@ var App = React.createClass({
     newState['hlColor'] = color;
     this.setState(newState);
   },
+  isConfigSet(config, value) {
+    return this.state[config] === value;
+  },
   render() {
     let {orientation, posY} = this.state;
   	let imageArray = [
@@ -54,30 +80,76 @@ var App = React.createClass({
   		'/img/land4.jpg',
   	];
     return (
-    <div style={{width: '100%', height: '60%', display: 'flex'}}>
-      <div style={{width: '50%', height: '100%'}}>
-    	 <Gallery config={this.state} images={imageArray} />
-      </div>
-      <div style={{width: '50%', padding: '0 1em'}}>
-        <h1>Config</h1>
-        {JSON.stringify(this.state)}
-        <h2>Layout</h2>
+      <div>
+      <h1 style={{width: '100%'}}>React Amazon Gallery</h1>
+      <div style={{width: '100%', height: '50%', display: 'flex', flexWrap: 'wrap', position: 'relative'}}>
+        <div style={{width: '50%', height: '100%'}}>
+          <WorkingGallery config={this.state} images={imageArray} />
+        </div>
+        <div style={{width: '50%', height: '100%', padding: '1em'}} className='configSection'>
+          <button className="btn btn-default" type="submit" onClick={this.resetDefault}>Reset</button>
           <div>
-            <h3>Orientation</h3>
-            <RaisedButton label="Vertical" disabled={orientation === 'vertical'} onClick={this.changeConfig.bind(null, 'orientation', 'vertical')} />
-            <RaisedButton label="Horizontal" disabled={orientation === 'horizontal'} onClick={this.changeConfig.bind(null, 'orientation', 'horizontal')} />
+         Container Height
+          <input
+            type="text"
+            value={this.state.cHeight}
+            onChange={this.handleTextChange.bind(null, 'cHeight')}
+          />
           </div>
           <div>
-            <h3>Position</h3>
-            <RaisedButton label="Top" disabled={posY === 'top'} onClick={this.changeConfig.bind(null, 'posY', 'top')} />
-            <RaisedButton label="Bottom" disabled={posY === 'bot'} onClick={this.changeConfig.bind(null, 'posY', 'bot')} />
+         Jewel Size
+          <input
+            type="text"
+            value={this.state.jewelSize}
+            onChange={this.handleTextChange.bind(null, 'jewelSize')}
+          />
           </div>
-          <h3>Jewel Size</h3>
-          <Slider defaultValue={this.state.jewelSize} onChange={this.handleSlider.bind(this, 'jewelSize')} />
-          <h3>Jewel Spacing</h3>
-          <Slider defaultValue={this.state.jewelSpacing} onChange={this.handleSlider.bind(this, 'jewelSpacing')} />
+          <div>
+         Jewel Spacing
+          <input
+            type="text"
+            value={this.state.jewelSpacing}
+            onChange={this.handleTextChange.bind(null, 'jewelSpacing')}
+          />
+          </div>
+          <div>
+         Highlight Color
+          <input
+            type="text"
+            value={this.state.hlColor}
+            onChange={this.handleTextChange.bind(null, 'hlColor')}
+          />
+          </div>
+          <div>
+         Highlight Size
+          <input
+            type="text"
+            value={this.state.hlSize}
+            onChange={this.handleTextChange.bind(null, 'hlSize')}
+          />
+          </div>
+          <div>
+              <h2>Layout</h2>
+              <button className={"btn btn-default"+(this.isConfigSet('orientation', 'vertical') ? ' active' : '')} type="submit" onClick={this.changeConfig.bind(null, 'orientation', 'vertical')}>Vertical</button>
+              <button className={"btn btn-default"+(this.isConfigSet('orientation', 'horizontal') ? ' active' : '')} type="submit" onClick={this.changeConfig.bind(null, 'orientation', 'horizontal')}>Horizontal</button>
+          </div>
+          <div>
+              <h2>Position</h2>
+              <button className={"btn btn-default"+(this.isConfigSet('posY', 'top') ? ' active' : '')} type="submit" onClick={this.changeConfig.bind(null, 'posY', 'top')}>Top</button>
+              <button className={"btn btn-default"+(this.isConfigSet('posY', 'bot') ? ' active' : '')} type="submit" onClick={this.changeConfig.bind(null, 'posY', 'bot')}>Bot</button>
+              <button className={"btn btn-default"+(this.isConfigSet('posX', 'left') ? ' active' : '')} type="submit" onClick={this.changeConfig.bind(null, 'posX', 'left')}>Left</button>
+              <button className={"btn btn-default"+(this.isConfigSet('posX', 'right') ? ' active' : '')} type="submit" onClick={this.changeConfig.bind(null, 'posX', 'right')}>Right</button>
+          </div>
+          Overlay 
+          <label>
+            <input name='overlay' checked={this.state.overlay} value={this.state.overlay} type="checkbox" onClick={this.changeConfig.bind(null, 'overlay', !this.state.overlay)} />
+          </label>
       </div>
-  	</div>
+    </div>
+      <div><h3>Config Object</h3></div>
+      <pre><code dangerouslySetInnerHTML={createMarkup('let config = '+library.json.prettyPrint(this.state)+';')}></code></pre>
+
+    </div>
     );
   }
 });
