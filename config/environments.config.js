2c5b051cd00de98842cace696de484b3f52f0dd6
Michael Lyons <mlyons000@gmail.com>
2017-06-16 20:46:33 -0700

    Fixed Webpack Production Public Path

--- config/environments.config.js
+++ config/environments.config.js
@@ -16,7 +16,7 @@ module.exports = {
   // Overrides when NODE_ENV === 'production'
   // ======================================================
   production : (config) => ({
-    compiler_public_path     : 'docs',
+    compiler_public_path     : 'dist',
     compiler_fail_on_warning : false,
     compiler_hash_type       : 'chunkhash',
     compiler_devtool         : null,
